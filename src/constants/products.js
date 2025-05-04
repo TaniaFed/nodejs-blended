8640978185fe86bf5cab087d5520dec85ec353b5
TaniaFed <ktv.poshta@gmail.com>
2025-05-03 23:22:50 -0400

    blended lesson 1 setup

---
+++ src/constants/products.js
@@ -0,0 +1,6 @@
+import path from 'node:path';
+
+export const PATH_DB = path.join( 'C:', 'Users', 'ktvpo', 'Documents', 'GitHub', 'Node.js', 'nodejs-blended', 'src', 'db', 'db.json');
+
+console.log(PATH_DB);
+
